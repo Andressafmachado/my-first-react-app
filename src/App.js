2658970a0fb29e9bb624135bcd0823918aa243f0
andressafmachado <andressamachado98@gmail.com>
2021-02-22 21:53:20 +0100

    Add 4 pokemons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Title from "./components/Title";
+import Pokemon from "./components/Pokemon";
 
 function App() {
+  const pokemons = [
+    {
+      name: "Charizard",
+      weight: 90,
+      awesome: true,
+      terrifying: false,
+      abilities: ["Blaze", "Solar power", "Tough claws", "Drought"],
+    },
+    {
+      name: "Bulbasaur",
+      weight: 6.9,
+      awesome: true,
+      terrifying: false,
+      abilities: ["Overgrow", "Chlorophyll"],
+    },
+    {
+      name: "Mewtwo",
+      weight: 122,
+      awesome: true,
+      terrifying: true,
+      abilities: ["Pressure", "Unnerve", "Steadfast", "Insomnia"],
+    },
+    {
+      name: "Mega beedrill",
+      weight: 65,
+      awesome: false,
+      terrifying: true,
+      abilities: ["Intimidate", "Unnerve"],
+    },
+  ];
   return (
     <div className="App">
       <main>
-        <Title />
+        <Title content="Some example title" />
+        {pokemons.map((pokemon, index) => (
+          <Pokemon
+            key={index}
+            name={pokemon.name}
+            weight={pokemon.weight}
+            awesome={pokemon.awesome}
+            terrifying={pokemon.terrifying}
+            abilities={pokemon.abilities}
+          />
+        ))}
       </main>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
